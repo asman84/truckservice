93f963967ffaf8516e00855fd6e52f7e26332b63
odildjamalov <odildjamalov@gmail.com>
2017-09-08 01:44:14 +0500

    new files added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = { lat: 41.304168, lng: 69.235773 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
